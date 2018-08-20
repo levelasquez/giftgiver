7dda4446ce9a71f5ef3e16b6d83ef86119bf84c8
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-20 16:15:25 -0300

    refactor with bdd

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,24 +2,32 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import App from './App'
 
-const app = shallow(<App />)
+describe('App component', () => {
+  const app = shallow(<App />)
 
-test('renders correctly', () => {
-  expect(app).toMatchSnapshot()
-})
+  test('renders correctly', () => expect(app).toMatchSnapshot())
 
-test('initializes the `state` with an empty list of gifts', () => {
-  expect(app.state().gifts).toEqual([])
-})
+  test('initializes the `state` with an empty list of gifts', () => {
+    const { gifts } = app.state()
 
-test('adds a new gift to `state` when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click')
+    expect(gifts).toEqual([])
+  })
 
-  expect(app.state().gifts).toEqual([{ id: 1 }])
-})
+  describe('when clicking the `add-gift` button', () => {
+    beforeEach(() => app.find('.btn-add').simulate('click'))
 
-test('adds a new gift to the rendered list when clicking the `add gift` button', () => {
-  app.find('.btn-add').simulate('click')
+    afterEach(() => app.setState({ gifts: [] }))
 
-  expect(app.find('.gift-list').children().length).toEqual(2)
+    test('adds a new gift to `state` when clicking the `add gift` button', () => {
+      const { gifts } = app.state()
+
+      expect(gifts).toEqual([{ id: 1 }])
+    })
+
+    test('adds a new gift to the rendered list when clicking the `add gift` button', () => {
+      const { length } = app.find('.gift-list').children()
+
+      expect(length).toEqual(1)
+    })
+  })
 })
